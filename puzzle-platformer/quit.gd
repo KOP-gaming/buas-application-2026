58009e81ed2621ff94363ad86000201328959e7b
KOP-gaming <owenkopetzky19@gmail.com>
2025-12-12 10:57:12 +0100

    menu fix + level one start
    - made the start of level 1
    - you can now continue from the tutorial to level 1
    - the entire start menu now works

--- puzzle-platformer/quit.gd
+++ puzzle-platformer/quit.gd
@@ -8,5 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("select"):
+	if button_pressed:
 		get_tree().quit()
--- puzzle-platformer/skip_tutorial.gd
+++ puzzle-platformer/skip_tutorial.gd
@@ -1,12 +1,12 @@
 extends Button
-var button = Button
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if button_pressed:
-		get_tree().change_scene_to_file("res://tutorial1.tscn")
+		get_tree().change_scene_to_file("res://level1.tscn")
